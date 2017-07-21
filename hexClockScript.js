9fbf0ecf9fe0fdf352af0b1b92d694a1049b6c59
Ivan Čerjan <ivancerjan3@gmail.com>
2017-07-21 21:15:15 +0200

    Add files via upload

---
+++ hexClockScript.js
@@ -0,0 +1,17 @@
+﻿function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+        "#" + h + "" + m + "" + s;
+    document.getElementById('obojajMe').style.backgroundImage = "linear-gradient(#" + h + "" + m + "" + s + ", white)";
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };
+    return i;
+}
