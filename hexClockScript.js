8a410a8eb77e2b05415db94a766847ad5f55fc1b
Ivan Čerjan <ivancerjan3@gmail.com>
2018-02-12 13:14:56 +0100

    Added parsing time
    Add time to max value of 255, and then parse to base 16

--- hexClockScript.js
+++ hexClockScript.js
@@ -6,8 +6,19 @@
     h = checkTime(h);
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-        "#" + h + "" + m + "" + s;
+
+	hh = Math.round(h*10.624);
+	hh = h.toString(16);
+	
+	mm = Math.round(m*4.25);
+	mm = m.toString(16);
+	
+	ss = Math.round(s*4.25);
+	ss = s.toString(16);
+	
+	document.getElementById('txt').innerHTML = h + ":" + m + ":" + s + "<br/>" + "#" + hh + ":" + mm + ":" + ss;
+		
+	
     document.getElementById('obojajMe').style.backgroundImage = "linear-gradient(#" + h + "" + m + "" + s + ", white)";
     var t = setTimeout(startTime, 500);
 }
